6b1b5228645c7690ac50e85b6fa59b844a2bfb67
DESKTOP-EKRVM5K\Student <christinalee22l4@gmail.com>
2022-07-22 09:22:03 +0800

    0722

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <ThemeContext.Provider value={themes.blue}>
+            <ThemeContext.Provider value={themes.dark}>
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<App />} />
